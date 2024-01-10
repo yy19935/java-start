f67dd6217a67061c4ed18688f764ace3851721b8
kimjoohwan <yy19935@gmail.com>
2024-01-11 01:09:35 +0900

    commentJava

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("hello Java1");  //hello java1을 출력합니다.
+        //System.out.println("hello java2"); 한 줄 주석 - 라인 전체 적용
+
+        /* 여러줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
