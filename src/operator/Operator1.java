f7f6bb853d615e12994a32e25e3918c27dcfe25a
kimjoohwan <yy19935@gmail.com>
2024-05-19 00:22:46 +0900

    operator

---
+++ src/operator/Operator1.java
@@ -0,0 +1,29 @@
+package operator;
+
+public class Operator1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+
+        // 덧셈
+        int sum = a + b;
+        System.out.println("a + b = " + sum);
+
+        // 뺄셈
+        int diff = a - b;
+        System.out.println("a - b = " + diff);
+
+        // 곱셈
+        int multi = a * b;
+        System.out.println("a * b = " + multi);
+
+        // 나눗셈
+        int div = a / b;
+        System.out.println("a / b = " + div);
+
+        // 나머지
+        int mod = a % b;
+        System.out.println("a % b = " + mod);
+    }
+}
